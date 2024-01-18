9c4530ab8d9b26962adbbdd41f365ef673b71486
KolyaKovt <109476933+KolyaKovt@users.noreply.github.com>
2024-01-18 15:39:32 +0000

    Deploying to gh-pages from @ KolyaKovt/goit-react-hw-05-movies@8ca315f0f787947ff56b7d79b0531415cafb021f 🚀

---
+++ assets/useData-opu8W5dG.js
@@ -0,0 +1,1 @@
+import{r as t}from"./index-Ssu_ML6R.js";const n=(e,r)=>{const[a,s]=t.useState(null),[o,u]=t.useState(null);return t.useEffect(()=>{e(r).then(s).catch(u)},[e,r]),[a,s,o]};export{n as u};
